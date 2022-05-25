c99ac29d39f8fae2f3f72f7f76c40b83ab52214e
Tobias Grassani <tobigrassani@gmail.com>
2022-05-25 14:47:33 -0300

    Se inserto objeto en lienzo canvas desde funciones.js

--- funciones.js
+++ funciones.js
@@ -1,3 +1,26 @@
-function empezarJuego() {
+var canvas = document.getElementById("juego");
+var ctx = canvas.getContext("2d");
 
+var x = canvas.width;
+var y = canvas.height;
+
+var personaje = {
+    alto:35,
+    ancho:20,
+    dibujar: function () {
+        ctx.beginPath();
+        ctx.rect(x/6, y/2, this.ancho, this.alto);
+        ctx.fillStyle = "black";
+        ctx.fill();
+        ctx.closePath();
+    }
+};
+function startGame () {
+    personaje.dibujar();
 }
+setInterval(dibujar, 0.1);
+
+
+
+
+
