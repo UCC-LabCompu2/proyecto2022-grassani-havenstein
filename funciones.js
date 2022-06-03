0b69fd99d4ba550aef5a2ec89e666622d7aad610
Tobias Grassani <tobigrassani@gmail.com>
2022-06-02 22:44:59 -0300

    Se activo cambio de fondo mediante opciones

--- funciones.js
+++ funciones.js
@@ -1,12 +1,13 @@
+var canvas = document.getElementById("juego");
+var ctx = canvas.getContext("2d");
+var x = canvas.width;
+var y = canvas.height;
+
 /**
  * Temporizador en lienzo canvas desde 3 hasta 1. Utilizado para indicar al jugador cuando iniciar
  * @method activarReloj
  */
 function activarReloj() {
-    var canvas = document.getElementById("juego");
-    var ctx = canvas.getContext("2d");
-    var x = canvas.width;
-    var y = canvas.height;
     var i=3;
     var reloj = setInterval(function () {
         canvas.width = canvas.width;
@@ -16,7 +17,7 @@ function activarReloj() {
         }
         else if(i===0){
             ctx.font = "50px Raleway";
-            ctx.fillText("GO", (x-50) / 2, y / 2); }
+            ctx.fillText("GO", (x-75) / 2, y / 2); }
         else {
             canvas.width = canvas.width;
         }
@@ -27,5 +28,29 @@ function activarReloj() {
     },1000,);
     reloj;
 }
-
+/**
+ * Cambia el fondo del canvas al seleccionar en una de las opciones
+ * @method cambiarFondo
+ * @param {string} img - Almacena el valor de la opcion seleccionada por el usuario
+ */
+function cambiarFondo(img) {
+    switch(img) {
+        case "gris":
+            canvas.style.background = '#eee';
+            canvas.style.backgroundSize = '100%';
+            break;
+        case "desierto":
+            canvas.style.background = 'url("imagenes/desierto.jpeg") no-repeat bottom';
+            canvas.style.backgroundSize = '250%';
+            break;
+        case "clasico":
+            canvas.style.background = 'url("imagenes/clasico.png") no-repeat bottom';
+            canvas.style.backgroundSize = '105%';
+            break;
+        case "ciudad":
+            canvas.style.background = 'url("imagenes/ciudad.png") no-repeat bottom';
+            canvas.style.backgroundSize = '130%';
+            break;
+    }
+}
 
